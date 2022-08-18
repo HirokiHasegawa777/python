0896d3fe404c0432306735f071aa35e2e0233a6f
HirokiHasegawa777 <s21C1094JW@s.chibakoudai.jp>
2022-08-18 20:17:20 +0900

    add_answer

---
+++ practice2.py
@@ -0,0 +1,15 @@
+A = int(input('Aさんの点数 : '))
+B = int(input('Bさんの点数 : '))
+i = 0
+j = 0
+print("A:", end = (''))
+while i < A:
+    print("]", end = (''))
+    i += 1
+print()
+
+print("B:", end = (''))
+while j < B:
+    print("]", end = (''))
+    j += 1
+print()
---
+++ practice4.py
@@ -0,0 +1,7 @@
+print("長方形")
+h = int(input("高さ："))
+w = int(input("横幅："))
+for i in range(w):
+    for j in range(h):
+        print("*", end = (""))
+    print()
